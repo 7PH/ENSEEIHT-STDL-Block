13f6e0ece92cc9973d25191885ea58741b4ddf30
7PH <b.raymond@protonmail.com>
2018-04-09 09:55:25 +0200

    Added a test description to write tests rapidly

--- tests/TAM.ts
+++ tests/TAM.ts
@@ -2,11 +2,18 @@ import * as child_process from "child_process";
 import * as fs from "fs";
 
 
+export interface TAMExpectedResult {
+    resolve: boolean;
+    checkType: boolean;
+    output?: string[];
+}
+
 export interface TAMResult {
     resolve: boolean;
     checkType: boolean;
     code: string;
     TAM: string;
+    output?: string[];
 }
 
 export class TAM {
@@ -16,7 +23,7 @@ export class TAM {
         return JSON.parse(shellResult.toString());
     }
 
-    public static execute(tam: string): string[] {
+    public static executeTam(tam: string): string[] {
         fs.writeFileSync('tmp.tam', tam);
         child_process.execSync("java -jar tools/aspartam.jar tmp.tam >/dev/null 2>&1");
         child_process.execSync("java -jar tools/tammachine.jar tmp.tamo > tmp.res 2>&1");
@@ -27,7 +34,37 @@ export class TAM {
         return r.split("\n");
     }
 
-    public static parseAndExecute(fileName: string): string[] {
-        return TAM.execute(TAM.parse(fileName).TAM);
+    public static parseAndExecute(fileName: string): TAMResult {
+        let parsed: TAMResult = TAM.parse(fileName);
+        parsed.output = TAM.executeTam(parsed.TAM);
+        return parsed;
+    }
+
+    public static ensureResult(expected: TAMExpectedResult, fileName: string): void {
+        let result: TAMResult = TAM.parseAndExecute(fileName);
+
+        if (expected.resolve !== result.resolve)
+            throw new Error("Resolve expected to be " + expected.resolve + " but is " + result.resolve);
+
+        if (expected.checkType !== result.checkType)
+            throw new Error("CheckType expected to be " + expected.checkType + " but is " + result.checkType);
+
+        if (expected.output != null && result.output == null)
+            throw new Error("Output expected to be " + expected.output + " but is null");
+
+        if (expected.output != null) {
+            if (result.output == null)
+                throw new Error("Output expected to be " + expected.output + " but is null");
+            if (result.output.length != expected.output.length)
+                throw new Error("Output expected to be " + expected.output + " but is " + result.output);
+
+            for (let i in expected.output) {
+                if (expected.output[i] != result.output[i]) {
+                    throw new Error("Output expected to be " + expected.output + " but is " + result.output);
+                }
+            }
+        }
+
+        return;
     }
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,28 +1,31 @@
 import {TAM} from "./TAM";
 
+
 describe('tests', function() {
 
     this.slow(500);
 
     describe('# print', function () {
         it('integer', function (done: any) {
-            let a: string[] = TAM.parseAndExecute('print.txt');
-
-            if (parseInt(a[0]) !== 11 || parseInt(a[1]) !== 20 || parseInt(a[2]) !== 15)
-                throw new Error("Invalid result - " + a.join(", "));
-
+            TAM.ensureResult({
+                resolve: true,
+                checkType: true,
+                output: ['11', '20', '15']
+            }, 'print.txt');
             done();
         });
     });
 
     describe('# ternary operator', function () {
         it('full test', function (done: any) {
-            let a: string[] = TAM.parseAndExecute('ternary.txt');
-
-            if (parseInt(a[0]) !== 1 || parseInt(a[1]) !== 3)
-                throw new Error("Invalid result - " + a.join(", "));
-
+            TAM.ensureResult({
+                resolve: true,
+                checkType: true,
+                output: ['1', '3']
+            }, 'ternary.txt');
             done();
         });
     });
+
+
 });
